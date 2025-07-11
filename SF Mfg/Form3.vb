e526d037853a751f8460f36a19ce13f1a62c4edb
dell <dell@DESKTOP-HAMR8R2>
2025-07-11 06:57:53 +0530

    changes

--- SF Mfg/Form3.vb
+++ SF Mfg/Form3.vb
@@ -153,7 +153,4 @@
         TextBox7.Text = total.ToString("0.##")
     End Sub
 
-    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
-
-    End Sub
 End Class
